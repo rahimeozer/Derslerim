d739443255670fcfb1abe80110cc6e9849a4fcce
rahimeozer <rahos_95@hotmail.com>
2024-05-21 21:44:16 +0300

    Sifre yonetim programı guncellendi

--- Python-Denemeleri/DosyaIslemleri/SifreYoneticisi.py
+++ Python-Denemeleri/DosyaIslemleri/SifreYoneticisi.py
@@ -1,26 +1,51 @@
 import os
+from cryptography.fernet import Fernet
+
 dosyaKonumu = "C:\\Users\\Rahos\\Documents\\Derslerim\\Python-Denemeleri\\DosyaIslemleri\\Sifre.txt"
 
+anahtarKonumu = "C:\\Users\\Rahos\\Documents\\Derslerim\\Python-Denemeleri\\DosyaIslemleri\\key.data"
+# Sadece Bir Kere 
+def anahtarOlustur():
+    key = Fernet.generate_key()
+    with open(anahtarKonumu,"wb")as dosya:
+        dosya.write(key)
+def anahtarOku():
+    with open(anahtarKonumu,"rb")as dosya:
+        key = dosya.read()
+        return key 
+    
 
-def tumunuGoruntule(DosyaYolu):
+
+    
+# print("Anahtar",key)
+
+anahtarOlustur()
+key  = anahtarOku()
+anahtar = Fernet(key)
+
+
+def tumunuGoruntule(dosyaYolu):
+    global anahtar
     if os.path.exists(dosyaKonumu):
        with open(dosyaYolu,"r")as dosya:
         icerik = dosya.readlines()
         
         for satir in icerik :
-            kullanıcıAdı,Sifre = satir.split(":")
-            print("Kullanıcı Adı : {},Sifre :{}".format(kullanıcıAdı,Sifre.replace("\n",""))) 
+            kullanıcıAdı,Sifre = satir.replace("\n","").split(":")
+            sifre=anahtar.decrypt(Sifre.encode()).decode()
+            print("Kullanıcı Adı : {},Sifre :{}".format(kullanıcıAdı,Sifre)) 
 
     else : 
         print("Dosya mevcut değil...")
 
 def kullanıcıEkle(DosyaYolu):
+    global anahtar
     if os.path.exists(DosyaYolu):
         yeniKullanıcıAdı = input("Kullanıcı Adı Giriniz :")
         yeniSifre = input("Şifre Giriniz :""\n")
+        yeniSifre= anahtar.encrypt(yeniSifre.encode()).decode()
         with open(DosyaYolu,"a")as dosya:
-            dosya.write(yeniKullanıcıAdı+":"+yeniSifre)
-
+            dosya.write(yeniKullanıcıAdı+":"+yeniSifre+ "\n")
     else:
         print("Dosya mevcut değil...")
         exit()    
@@ -29,6 +54,12 @@ def kullanıcıSil(DosyaYolu):
     if os.path.exists(DosyaYolu):
         kullanıcıAdı = input("Silinecek kullanıcı adı giriniz :")
         with open (DosyaYolu,"r") as dosya:
+           Tümİcerik = dosya.read()
+           if kullanıcıAdı not in Tümİcerik:
+               print("Kullanıcı bulunamadı..")
+               return
+           
+           dosya.seek(0)
            icerik = dosya.readlines()
 
         for i,satir in enumerate (icerik) :
@@ -38,17 +69,42 @@ def kullanıcıSil(DosyaYolu):
         with open (DosyaYolu,"w")as dosya:
              dosya.writelines(icerik)
 
-
-
     else :
-        print("Dosya mevcut değil..")                 
-    
+        print("Dosya mevcut değil..")  
+
+def SifreDegistirme(path):
+    global anahtar
+    if os.path.exists(path):
+        kullanıcıAdı=input("Şifresi değiştirilecek kullanıcı adını giriniz :")
+        with open(path,"r")as dosya:
+            Tümicerik = dosya.read()
+            if kullanıcıAdı not in Tümicerik:
+                print("Kullanıcı bulunamadı")
+                return
+            
+            dosya.seek(0)
+            icerik = dosya.readlines()
+
+    for idx ,satir in enumerate (icerik) :
+        if kullanıcıAdı in satir:
+            kullanıcıAdı, sifre = satir.replace("\n","").split(":")
+            sifre = input("Yeni şifre giriniz : ")
+            sifre = anahtar.encrypt (sifre.encode()).decode()
+            icerik[idx] = kullanıcıAdı + ":" +sifre +"\n"
+            break
+        # print( "Şifre başarı ile değiştirildi")
+
+        with open (path,"w") as dosya:
+            dosya.writelines(icerik)
+
+       
 print("Secenekleriniz :\n"
       "1.Kullanıcı ve Sifre Görüntüle\n"
       "2.Kullanıcı Ekle\n"
       "3.Kullanıcı Silme\n"
       "4.Sifre Değistirme\n"
-      "5.Cıkıs\n"
+      "5.Anahtar Oluştur\n"
+      "6.Cıkıs\n"
       "İslem >:")
 cevap = input("Bir Secenek seciniz :")
 
@@ -62,10 +118,13 @@ elif cevap =="3":
     kullanıcıSil(dosyaKonumu)
     #KullanıcıSilme
 elif cevap =="4":
-    None
+    SifreDegistirme(dosyaKonumu)
     #SifreDegistirme
 elif cevap ==  "5":
-    None
-    exit()
+    anahtarOlustur()
+    key = anahtarOku()
+    anahtar = Fernet(key)
+elif cevap == "6":
+    exit()    
 else:
     print("DALGA MI GEÇİYON ADAM MI SEÇİYON KARDEŞİMM...")          
